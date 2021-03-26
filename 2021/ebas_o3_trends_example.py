054bf23a247b3514f7b40126bae286d9d4a7970a
jgliss <jonasg@met.no>
2021-03-26 18:35:19 +0100

    Add simple example for O3 max trend

--- 2021/ebas_o3_trends_example.py
+++ 2021/ebas_o3_trends_example.py
@@ -22,3 +22,48 @@ else:
 reader = pya.io.ReadUngridded('EBASMC', data_dir=data_dir)
 
 conco3 = reader.read(vars_to_retrieve='conco3')
+
+all_sites = conco3.to_station_data_all('conco3', start=2000,
+                                       stop=2020)
+
+# EVERYTHING BELOW COULD GO IN A LOOP OVER all_sites['stats']
+
+first_station = all_sites['stats'][0]
+
+print(first_station)
+
+min_num_obs = pya.const.OBS_MIN_NUM_RESAMPLE # roughly 25% coverage requirement
+
+print('Resampling coverage requirement:')
+print(min_num_obs)
+
+# O3 max from hourly data
+resample_how = 'max'
+
+# make sure data is hourly (not necessarily all are)
+assert first_station.ts_type == 'hourly'
+
+# reample to daily and use max aggregator to get O3 max
+first_station_d = first_station.resample_time('conco3', 'daily',
+                                              apply_constraints=True,
+                                              min_num_obs=min_num_obs,
+                                              how=resample_how,
+                                              inplace=False)
+
+
+te = pya.trends_engine.TrendsEngine()
+
+te.var_name = 'conco3'
+tseries = first_station_d['conco3']
+# this contains all that is there between 2000 -> 2020
+te.daily = tseries
+
+# e.g. compute trend for 2005 -> 2015
+trend = te.compute_trend(2005, 2015)
+
+te.plot(season='all')
+
+
+
+
+
